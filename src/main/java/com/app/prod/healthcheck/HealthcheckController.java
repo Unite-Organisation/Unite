3a5441916a7bbe7412740a67697c2d8ce3103a6c
Dorajn <derquv@gmail.com>
2025-08-03 13:13:13 +0200

    init/v2

--- src/main/java/com/app/prod/healthcheck/HealthcheckController.java
+++ src/main/java/com/app/prod/healthcheck/HealthcheckController.java
@@ -1,17 +1,23 @@
 package com.app.prod.healthcheck;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Clock;
+import java.time.LocalTime;
+
 @RestController
 @RequestMapping("healthcheck")
+@RequiredArgsConstructor
 public class HealthcheckController {
+
+    private final Clock clock;
 
     @GetMapping()
     public ResponseEntity<String> healthCheck(){
-        return ResponseEntity.ok("App stable and running.");
+        return ResponseEntity.ok(String.format("[%s] App stable and running.", LocalTime.now(clock)));
     }
-
 }
