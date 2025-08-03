7071a65c4f941a2979aa14b41c8fba1fa88f7d08
Dorajn <derquv@gmail.com>
2025-08-03 13:45:06 +0200

    modified init 0_0_0 flyway migration - added tables needed for chat

--- src/main/resources/db/migration/V0_0_0__init.sql
+++ src/main/resources/db/migration/V0_0_0__init.sql
@@ -1,8 +1,40 @@
 CREATE TABLE users
 (
-
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name VARCHAR(20) NOT NULL,
-    last_name  VARCHAR(20) NOT NULL
+    last_name  VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE conversations
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    is_group BOOLEAN NOT NULL,
+    name VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE conversation_member
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    conversation_id UUID REFERENCES conversations(id) ON DELETE CASCADE,
+    join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE message
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    sender_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    conversation_id UUID REFERENCES conversations(id) ON DELETE CASCADE,
+    content TEXT,
+    send_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE messages_read
+(
+    message_id UUID REFERENCES message(id) ON DELETE CASCADE,
+    viewed_by UUID REFERENCES users(id) ON DELETE CASCADE,
+    viewed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(message_id, viewed_by)
 );
